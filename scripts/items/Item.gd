ed6eafb50108da01b9928ab741e3d8a2c45a8cac
Diego Melo <diego.jackchan@gmail.com>
2024-07-22 15:04:48 -0300

    feat: init of the core mechanic

--- scripts/items/Item.gd
+++ scripts/items/Item.gd
@@ -1,5 +1,12 @@
 class_name Item
 extends Node
 
+signal fruit_collected(fruit)
+
 func _ready():
-	add_to_group("items")
+	pass
+	#add_to_group("items")
+
+func _on_body_entered(_body):
+	fruit_collected.emit(name)
+	queue_free()
--- scripts/level/level_1.gd
+++ scripts/level/level_1.gd
@@ -3,12 +3,12 @@ extends Node
 @onready var level_machine : LevelMachine = $LevelMachine
 
 const POSITIONS = [
-  Vector2(101,190),
-  Vector2(180,140),
-  Vector2(310,205),
-  Vector2(390,230),
-  Vector2(415,50),
-  Vector2(78,40),
+  Vector2(62.207,143.368),
+  Vector2(138.911,98.503),
+  Vector2(267.341,160.097),
+  Vector2(351.112,185.01),
+  Vector2(376.767,9.891),
+  Vector2(38.388,-4.491),
 ]
 
 var fruits_scenes: Dictionary = {
@@ -21,17 +21,42 @@ var fruits_scenes: Dictionary = {
   "pineapple": preload("res://scenes/items/pineapple.tscn"),
   "strawberry": preload("res://scenes/items/strawberry.tscn"),
 }
+var response: Array = []
+var collected: Array = []
+var answers: Array = []
+var answers_time: Array = []
+var time: float = 0.0
 
 func _ready():
 	var selected_fruits: Array = []
-	var response: Array = []
 	selected_fruits = level_machine.get_fruits()
 	response = level_machine.get_ordered_fruits(selected_fruits)
 	load_fruits(selected_fruits)
-
+	
+func _process(delta: float) -> void:
+	time+=delta
+	
 func load_fruits(selected_fruits: Array) -> void:
 	for fruit in selected_fruits:
-		var obj = fruits_scenes.get(fruit).instantiate()
+		var scene = fruits_scenes.get(fruit).instantiate()
+		scene.connect("fruit_collected", _on_fruit_collected)
 		var idx = selected_fruits.find(fruit)
-		obj.set_position(POSITIONS[idx])
-		get_parent().add_child.call_deferred(obj)
+		scene.set_position(POSITIONS[idx])
+		add_child.call_deferred(scene)
+	
+func _on_fruit_collected(fruit):
+	collected.append(fruit.to_lower())
+	answers_time.append(time)
+	
+	if collected.size() == 6:
+		check_if_all_colected()
+
+func check_if_all_colected():
+	for fruit in collected:
+		var idx = response.find(fruit)
+		if collected[idx] == response[idx]:
+			answers.append(true)
+		else:
+			answers.append(false)
+	print(", ".join(answers))
+	print(", ".join(answers_time))
